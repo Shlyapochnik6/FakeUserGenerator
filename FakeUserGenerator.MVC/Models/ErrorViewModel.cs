5da6284534d6eb002287bd0259adfe7b9b88f585
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-11-21 18:39:56 +0300

    Initial commit

---
+++ FakeUserGenerator.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FakeUserGenerator.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">Task5</h1>
+
+<div>
+    "Task5" was created for Itransition
+</div>
